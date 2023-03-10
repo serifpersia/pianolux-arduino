252386a71ddb15ea228d6f98322a9641931235d6
Serif <62844718+serifpersia@users.noreply.github.com>
2023-03-10 11:36:22 +0100

    Changed name to ArduinoPianoLED

---
+++ ArduinoPianoLED/FadingRunEffect.h
@@ -0,0 +1,31 @@
+//FadingRunEffect.h
+#ifndef FadingRunEffect_h
+#define FadingRunEffect_h
+
+#include <Arduino.h>
+
+class FadingRunEffect {
+
+public:
+  FadingRunEffect(int effectLen, int startPosition, int hue, int saturation, int headFadeRate, int velocity);
+  void nextStep();
+  boolean finished();
+  int getSaturation(int velocity);
+  int getBrightness(int velocity);
+  void setHeadLED(int step);
+  int getSteps();
+  int adjustValue(int value, int lowerThreshold, int maxValue);
+  int calcOffset(int step, int velocity);
+  
+private:
+  int effectLen;
+  int startPosition;
+  int step;
+  int hue;
+  int saturation;
+  int headFadeRate;
+  int velocity;
+  unsigned long lastUpdate;
+};
+
+#endif
