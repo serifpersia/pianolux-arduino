7d6c1713b7b647b2a781ab3c5a79b370227c87ef
Serif <62844718+serifpersia@users.noreply.github.com>
2023-07-14 19:28:39 +0200

    update bump

--- PianoLED/src/com/serifpersia/pianoled/Updater.java
+++ PianoLED/src/com/serifpersia/pianoled/Updater.java
@@ -31,7 +31,7 @@ public class Updater {
 	String os = System.getProperty("os.name").toLowerCase();
 
 	private boolean debugJSONOff = true;
-	public String VersionTag = "v4.2.3";
+	public String VersionTag = "v4.2.4";
 	String VersionFile;
 
 	public String getDownloadUrl(JsonNode latestRelease, String fileName) throws IOException {
