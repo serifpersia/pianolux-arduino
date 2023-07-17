63f6edfbdb359a410be01824ddba103fcb315ade
Serif <62844718+serifpersia@users.noreply.github.com>
2023-07-17 10:40:00 +0200

    Update LivePlayPanel.java

--- PianoLED/src/com/serifpersia/pianoled/liveplay/LivePlayPanel.java
+++ PianoLED/src/com/serifpersia/pianoled/liveplay/LivePlayPanel.java
@@ -35,7 +35,6 @@ public class LivePlayPanel extends JPanel{
 //	private MidiRecorder midiRecorder;
 	private LiveRoll liveRoll;
 	private JPanel slideControlsPane = new JPanel();
-	private JButton lbPlayMidi = new JButton("▶");
 	private JCheckBox gridToggle;
 	private PianoLED pianoLED;
 	private JCheckBox infoToggle;
@@ -129,146 +128,46 @@ public class LivePlayPanel extends JPanel{
 		controlsPane.setBackground(SLIDING_PANEL_BG);
 		slideControlsPane.add(controlsPane, BorderLayout.EAST);
 		GridBagLayout gbl_controlsPane = new GridBagLayout();
-		gbl_controlsPane.columnWidths = new int[] { 25, 0, 0, 0 };
-		gbl_controlsPane.rowHeights = new int[] { 143, 0, -14, 30, 94, 36, 0 };
-		gbl_controlsPane.columnWeights = new double[] { 1.0, 0.0, 0.0, 0.0 };
-		gbl_controlsPane.rowWeights = new double[] { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE };
+		gbl_controlsPane.columnWidths = new int[] { 170 };
+		gbl_controlsPane.rowHeights = new int[] { 77, -18, -33, 0 };
+		gbl_controlsPane.columnWeights = new double[] { 1.0 };
+		gbl_controlsPane.rowWeights = new double[] { 0.0, 0.0, 0.0, Double.MIN_VALUE };
 		controlsPane.setLayout(gbl_controlsPane);
 
-		addPlaybackControls(controlsPane);
 		addUIControls(controlsPane, 3);
 
 	}
 
 	private int addUIControls(JPanel controlsPane, int gridy) {
 		gridToggle = new JCheckBox("Show Grid");
+		gridToggle.setHorizontalAlignment(SwingConstants.CENTER);
 		gridToggle.setBackground(SLIDING_PANEL_BG);
 		gridToggle.setForeground(Color.WHITE);
 		gridToggle.setFont(SLIDING_PANEL_FONT);
-
-		GridBagConstraints gbc = new GridBagConstraints();
-		gbc.gridwidth = 4;
-		gbc.insets = new Insets(5, 5, 5, 0);
-		gbc.fill = GridBagConstraints.HORIZONTAL;
-		gbc.gridx = 0;
-		gbc.gridy = 4;
-		controlsPane.add(gridToggle, gbc);
+		
+				GridBagConstraints gbc = new GridBagConstraints();
+				gbc.insets = new Insets(5, 5, 5, 0);
+				gbc.fill = GridBagConstraints.HORIZONTAL;
+				gbc.gridx = 0;
+				gbc.gridy = 1;
+				controlsPane.add(gridToggle, gbc);
+				gbc.gridy = gridy + 1;
 
 		infoToggle = new JCheckBox("Tech Info");
+		infoToggle.setHorizontalAlignment(SwingConstants.CENTER);
 		GridBagConstraints gbc_InfoToggle = new GridBagConstraints();
 		infoToggle.setBackground(SLIDING_PANEL_BG);
 		infoToggle.setForeground(Color.WHITE);
 		infoToggle.setFont(SLIDING_PANEL_FONT);
-		gbc_InfoToggle.insets = new Insets(0, 0, 0, 5);
-		gbc_InfoToggle.gridwidth = 2;
 		gbc_InfoToggle.gridx = 0;
-		gbc_InfoToggle.gridy = 5;
+		gbc_InfoToggle.gridy = 2;
 		controlsPane.add(infoToggle, gbc_InfoToggle);
-		gbc.gridy = gridy + 1;
 		return gridy;
 	}
 
 
-	private void addPlaybackControls(JPanel controlsPane) {
-		Font midiControlsFont = new Font("Symbola", Font.BOLD, 16);
 
-//		JButton lbStart = new JButton("⏮");
-//		GridBagConstraints gbc_lbStart = new GridBagConstraints();
-//		lbStart.setBackground(new Color(52, 152, 219));
-//		lbStart.setForeground(Color.WHITE);
-//		lbStart.setFocusable(false);
-//		lbStart.setBorderPainted(false);
-//		lbStart.setToolTipText("Restart Playback");
-//		gbc_lbStart.anchor = GridBagConstraints.NORTH;
-//		gbc_lbStart.insets = new Insets(0, 0, 5, 5);
-//		gbc_lbStart.gridx = 0;
-//		gbc_lbStart.gridy = 4;
-//		lbStart.setFont(midiControlsFont);
-//		controlsPane.add(lbStart, gbc_lbStart);
-//		lbStart.addActionListener(new ActionListener() {
-//			@Override
-//			public void actionPerformed(ActionEvent e) {
-//				midiRecorder.rewind();
-//			}
-//		});
 
-//		JButton lbGoBack = new JButton("⏪");
-//		GridBagConstraints gbc_lbGoBack = new GridBagConstraints();
-//		lbGoBack.setBackground(new Color(52, 152, 219));
-//		lbGoBack.setForeground(Color.WHITE);
-//		lbGoBack.setFocusable(false);
-//		lbGoBack.setBorderPainted(false);
-//		lbGoBack.setToolTipText("Previous");
-//		gbc_lbGoBack.anchor = GridBagConstraints.NORTH;
-//		gbc_lbGoBack.insets = new Insets(0, 0, 5, 5);
-//		gbc_lbGoBack.gridx = 1;
-//		gbc_lbGoBack.gridy = 4;
-//		lbGoBack.setFont(midiControlsFont);
-//		controlsPane.add(lbGoBack, gbc_lbGoBack);
-//		lbGoBack.addActionListener(new ActionListener() {
-//			@Override
-//			public void actionPerformed(ActionEvent e) {
-//				midiRecorder.rewind(-PLAYER_REWIND_SEC);
-//			}
-//		});
-
-		GridBagConstraints gbc_lbPlayMidi = new GridBagConstraints();
-		lbPlayMidi.setBackground(new Color(52, 152, 219));
-		lbPlayMidi.setForeground(Color.WHITE);
-		lbPlayMidi.setFocusable(false);
-		lbPlayMidi.setBorderPainted(false);
-		lbPlayMidi.setToolTipText("Play/Pause");
-		gbc_lbPlayMidi.anchor = GridBagConstraints.NORTH;
-		gbc_lbPlayMidi.insets = new Insets(0, 0, 5, 5);
-		gbc_lbPlayMidi.gridx = 2;
-		gbc_lbPlayMidi.gridy = 4;
-		lbPlayMidi.setFont(midiControlsFont);
-		controlsPane.add(lbPlayMidi, gbc_lbPlayMidi);
-		lbPlayMidi.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				liveRoll.start();
-//				if( midiRecorder == null )
-//				{
-//					initMidiRecorder(pianoLED.getPianoController().getMidiDevice());
-//				}
-// 				midiRecorder.startStopRecording();
-//				if (midiRecorder.isRecording()) {
-//					setPlaybackButtonPlay();
-//				} else {
-//					setPlaybackButtonStop();
-//				}
-			}
-		});
-//
-//		JButton lbGoForwards = new JButton("⏩");
-//		GridBagConstraints gbc_lbGoForwards = new GridBagConstraints();
-//		lbGoForwards.setBackground(new Color(52, 152, 219));
-//		lbGoForwards.setForeground(Color.WHITE);
-//		lbGoForwards.setFocusable(false);
-//		lbGoForwards.setBorderPainted(false);
-//		lbGoForwards.setToolTipText("Next");
-//		gbc_lbGoForwards.anchor = GridBagConstraints.NORTH;
-//		gbc_lbGoForwards.insets = new Insets(0, 0, 5, 0);
-//		gbc_lbGoForwards.gridx = 3;
-//		gbc_lbGoForwards.gridy = 4;
-//		lbGoForwards.setFont(midiControlsFont);
-//		controlsPane.add(lbGoForwards, gbc_lbGoForwards);
-//		lbGoForwards.addActionListener(new ActionListener() {
-//			@Override
-//			public void actionPerformed(ActionEvent e) {
-//				midiRecorder.rewind(5);
-//			}
-//		});
-	}
-
-	private void setPlaybackButtonStop() {
-		lbPlayMidi.setText("▶");
-	}
-
-	private void setPlaybackButtonPlay() {
-		lbPlayMidi.setText("⏸");
-	}
 
 	public boolean drawLines() {
 
